9e51ead15ba4e357b09a1c91dffe5bb79563ba19
khalil-elqaddabi <kahlilelqaddabi9@gmail.com>
2026-03-05 03:43:22 +0000

    feat: add counts to dashbord for statistic

--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -1,18 +1,24 @@
 <?php
 
-namespace App\Http\Controllers\admin;
+namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Colocation;
+use App\Models\Expense;
+use App\Models\Payment;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-     public function dashboard()
+    public function dashboard()
     {
-        $userCount   = user::count();
-        
-
-        return view('admin.dashboard', compact('userCount'));
+        return view('admin.dashboard', [
+            'userCount'     => User::count(),
+            'activeUsers'    => User::whereNull('banned_at')->whereNotNull('email_verified_at')->count(),
+            'colocCount' => Colocation::count(),
+            'totalExpenses'  => Expense::count(),
+            'totalPayments' => Payment::count(),
+        ]);
     }
 }
